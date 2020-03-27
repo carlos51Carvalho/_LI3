a9a0182f80ce41bc04c83f69bd60d381749ab8d6
ZeCosta <josepedroc.costa@gmail.com>
2020-03-27 18:26:49 +0000

    Correção de incompatibilidades nos ficheiros

--- grupo52/projC/src/main.c
+++ grupo52/projC/src/main.c
@@ -17,7 +17,6 @@ int main(){
 	TVendas *vendas = NULL;
 	int p=0;
 	int v=0;
-<<<<<<< HEAD
 	int c=0;
 	char letra = 'D';
 	//char *cl = "H4511" ;
@@ -49,30 +48,6 @@ int main(){
 	//printf("O ultimo elemento dos clientes é : %s\n", ultimoElemTB(cliente));
 	//printf("O ultimo elemento dos produtos é : %s\n", ultimoElemTB(prod));
 	//printf("O ultimo elemento das vendas é : %s\n", (*vendas +(7*(v)+0)));
-=======
-	
-	
-	c = ler_clientes(&cliente ,SIZE_CL);
-	quicksortc(cliente,c);
-	//imprimecliente(cliente,c);
-	//ultimo cliente
-	//printf("\n%s\n", cliente[c-1]);
-	//Quantos clientes começam pela letra ?
-	//printf("%d\n", letra_cl(cliente,"B",c) );
-	printf("\n\n%d===============================================================\n",c);
-	p = ler_prod (&prod , SIZE_PR);
-	quicksortc(prod,p);
-	//imprimeprod(prod,p);
-	printf("%d===============================================================\n",p);
-	v = ler_venda(&vendas,SIZE_VD,cliente,prod,c,p);
-	//imprimevendas(vendas,v);
-	printf("%d===============================================================\n",v);
-	printf("%d\n", vendas_1c(vendas,v,"H4511"));
-	printf("Vendas filial 1 .... %d\n", vendas_fil1(vendas,v,1));
-	printf("Vendas filial 2 .... %d\n", vendas_fil1(vendas,v,2));
-
-	escrever(vendas,"vendasmm.txt",v);
->>>>>>> 42add232be9b05b1817bf6959b7ed44623f85761
 
 	free(cliente);
 	free(prod);
