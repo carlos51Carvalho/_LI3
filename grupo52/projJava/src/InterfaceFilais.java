fe3fdfdaaf8de903cbfc98544e3194a0f5df1586
ZeCosta <josepedroc.costa@gmail.com>
2020-05-16 00:09:19 +0100

    Alteracao na Interface de filiais

--- grupo52/projJava/src/InterfaceFilais.java
+++ grupo52/projJava/src/InterfaceFilais.java
@@ -5,7 +5,6 @@ public interface InterfaceFilais {
 
     boolean existeProd(String cl, int ip, int f, int m, String p);
     void acrescentaFil(String[] cl);
-    List<ClFil> getArr(int i);
     void addCl(String cl);
     void addCls(TreeSet<String> clientes);
     int numeroComparadores();
