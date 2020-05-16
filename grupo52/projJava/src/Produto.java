aed6b469b0c3526bec02e9987aee5bf9584c12a4
homeless51 <carlosgba1@gmail.com>
2020-05-16 19:04:00 +0100

    alteraçao na classe Cliente

--- grupo52/projJava/src/Produto.java
+++ grupo52/projJava/src/Produto.java
@@ -8,9 +8,24 @@
 public class Produto
 {
     private String produto;
-    
+
+
+    public static boolean validaNum(String p) {
+        boolean r = true;
+        for (int i = 2; i < 6 && r; i++) {
+            if (i == 2) {
+                if (p.charAt(i) < '1' && p.charAt(i) > '9') r = false;
+                else {
+                    if (p.charAt(i) < '0' && p.charAt(i) > '9') r = false;
+                }
+            }
+        }
+        return r;
+    }
+
+
     public static boolean validaProduto(String c){
-        return true;
+        return c.charAt(0) >= 'A' && c.charAt(0) <= 'Z' && c.charAt(1) >= 'A' && c.charAt(1) <= 'Z'&& validaNum(c);
     }
     public static int hashString(String c){
         return c.charAt(0) - 'A';
